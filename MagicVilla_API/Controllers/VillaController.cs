e8b5a6baba6539bec64f17e9f304719dc8eb1fb6
Elfego <eapen.b@outlook.es>
2023-11-15 00:22:44 -0600

    Agregando los primeros end points

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,25 @@
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")] // ruta
+    [ApiController] // controlador API
+    public class VillaController : ControllerBase
+    {
+        [HttpGet] //tipo de verbo
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return VillaStore.villaList;
+          
+        }
+        [HttpGet( "id")]
+        public VillaDto GetVilla(int id) 
+        {
+            return VillaStore.villaList.FirstOrDefault(v=>v.Id==id);
+        }
+    }
+}
