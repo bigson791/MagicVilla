47d3abcd1d02f8288cde61707d1e2f1c7dd3b475
eaapen <eaapen@INF30>
2024-02-06 13:16:31 -0600

    actualizando el proyecto

--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -1,3 +1,6 @@
+using MagicVilla_API.Datos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,10 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//configurando la opcion de la base de datos
+builder.Services.AddDbContext<ApplicationDbContext>(option => {
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}); //agrando el contexto para la inyeccion de dependencias
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
