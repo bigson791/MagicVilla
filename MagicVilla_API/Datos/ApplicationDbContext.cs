1e2686afca9a9b79e2516dead32437d862379241
eaapen <eaapen@INF30>
2024-02-07 14:21:32 -0600

    agregando la opcion migracion

--- MagicVilla_API/Datos/ApplicationDbContext.cs
+++ MagicVilla_API/Datos/ApplicationDbContext.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MagicVilla_API.Modelos;
 
 namespace MagicVilla_API.Datos
 {
     public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+                
+        }
+
         public DbSet<Villa> Villas { get; set; }
     }
 }
