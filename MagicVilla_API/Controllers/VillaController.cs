9f6970e6890411cc5d649c7a207aee281aa8937c
Elfego <eapen.b@outlook.es>
2024-02-07 23:07:08 -0600

    agregando la base de datos a la api

--- MagicVilla_API/Controllers/VillaController.cs
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -14,11 +14,13 @@ namespace MagicVilla_API.Controllers
     {
 
         // inyeccion de la dependencia
-        private readonly ILogger<VillaController> _logger;   
+        private readonly ILogger<VillaController> _logger;
+        private readonly ApplicationDbContext _db;
         //se crea un constructor con ctor
-        public VillaController(ILogger<VillaController> logger)
+        public VillaController(ILogger<VillaController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
         // inyeccion de la dependencia 
 
@@ -30,7 +32,7 @@ namespace MagicVilla_API.Controllers
         {
             //con loger se puede enviar mensajes de error.
             _logger.LogInformation("Obtener las villas");
-            return Ok(VillaStore.villaList);
+            return Ok(_db.Villas.ToList()); //_db.Villas.ToList() es como hacer un select * from
 
         }
         [HttpGet("id: int", Name = "GetVilla")]
@@ -44,7 +46,8 @@ namespace MagicVilla_API.Controllers
                 _logger.LogInformation("Error al obtener la villa"+id);
                 return BadRequest(); // se retorna el error
             }
-            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+            //var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+            var villa = _db.Villas.FirstOrDefault(v => v.Id == id);
 
             if (villa == null) // verificando que el no exista
             {
@@ -69,7 +72,7 @@ namespace MagicVilla_API.Controllers
                 return BadRequest(ModelState);
             }
 
-            if (VillaStore.villaList.FirstOrDefault(v => v.Nombre.ToLower() == villaDto.Nombre.ToLower()) != null)
+            if (_db.Villas.FirstOrDefault(v => v.Nombre.ToLower() == villaDto.Nombre.ToLower()) != null)
             {
                 ModelState.AddModelError("NombreExiste", "la villa con ese nombre ya existe!");
                 return BadRequest(ModelState);
@@ -85,8 +88,22 @@ namespace MagicVilla_API.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
-            villaDto.Id = VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault().Id + 1;
-            VillaStore.villaList.Add(villaDto);
+            Villa modelo = new()
+            {
+                Id = villaDto.Id,
+                Nombre = villaDto.Nombre,
+                Detalle = villaDto.Detalle,
+                ImagenUrl = villaDto.ImagenUrl,
+                Ocupantes = villaDto.Ocupantes,
+                Tarifa = villaDto.Tarifa,
+                MetrosCuadrados = villaDto.MetrosCuadrados,
+                Amenidad = villaDto.Amenidad,
+
+            };
+            _db.Villas.Add(modelo);
+            _db.SaveChanges();
+           // villaDto.Id = VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault().Id + 1;
+           // VillaStore.villaList.Add(villaDto);
             //cuando en la api se crea un nuevo recurso se debe indicar la url de este
             // el primer parametro llama al endpoint get, el segundo parametro es el id que nos pide el endopoint y el tercer parametro es todo el objeto 
             return CreatedAtRoute("GetVilla", new { id = villaDto.Id }, villaDto);
@@ -106,7 +123,7 @@ namespace MagicVilla_API.Controllers
             {
                 return BadRequest(); // se retorna el error
             }
-            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+            var villa = _db.Villas.FirstOrDefault(v => v.Id == id);
 
             if (villa == null) // verificando que el no exista
             {
@@ -114,10 +131,42 @@ namespace MagicVilla_API.Controllers
             }
 
             // se elimina la villa de la lista
-            VillaStore.villaList.Remove(villa);
+            _db.Villas.Remove(villa);
+            _db.SaveChanges();
+            //VillaStore.villaList.Remove(villa);
 
             return NoContent();
         }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult UpdateVilla(int id, [FromBody] villaDto villaDto)
+        {
+            if (villaDto == null || id != villaDto.Id)
+            {
+                return BadRequest();
+            }
+
+            Villa modelo = new()
+            {
+                Id = villaDto.Id,
+                Nombre = villaDto.Nombre,
+                Detalle = villaDto.Detalle,
+                ImagenUrl = villaDto.ImagenUrl,
+                Ocupantes = villaDto.Ocupantes,
+                Tarifa = villaDto.Tarifa,
+                MetrosCuadrados = villaDto.MetrosCuadrados,
+                Amenidad = villaDto.Amenidad,
+            }
+
+            _db.Villas.Update(modelo);
+            _db.SaveChanges();
+            return NoContent();
+        }
+
+
 
 
         //permite actualizar un solo objeto, para este caso se usa en path se usa /nombre, en op se usa "replace" en value se agrega el valor que se desea, 
--- MagicVilla_API/Datos/ApplicationDbContext.cs
+++ MagicVilla_API/Datos/ApplicationDbContext.cs
@@ -11,5 +11,39 @@ namespace MagicVilla_API.Datos
         }
 
         public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Villa>().HasData(
+               new Villa()
+                {
+                    Id = 1,
+                    Nombre = "Villa Real",
+                    Detalle = "Detalle de la villa...",
+                    ImagenUrl = "",
+                    Ocupantes = 5,
+                    MetrosCuadrados = 50,
+                    Tarifa = 200,
+                    Amenidad = "",
+                    FechaCreacion = DateTime.Now,
+                    FechaActualizacion = DateTime.Now
+                },
+               new Villa()
+                {
+                    Id = 2,
+                    Nombre = "Premium Vista a la piscina",
+                    Detalle = "Detalle de la villa...",
+                    ImagenUrl = "",
+                    Ocupantes = 4,
+                    MetrosCuadrados = 40,
+                    Tarifa = 150,
+                    Amenidad = "",
+                    FechaCreacion = DateTime.Now,
+                    FechaActualizacion = DateTime.Now
+                }
+              );
+
+        }
     }
 }
