9e71137da72510d1521665e6304f1d1c057f3154
Elfego <eapen.b@outlook.es>
2024-02-28 22:08:12 -0600

    Se agragaron las interfaces

---
+++ MagicVilla_API/Repositorio/IRepositorio/IRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace MagicVilla_API.Repositorio.IRepositorio
+{
+    //<T> where T : class hace referencia a una interfaz generica
+    public interface IRepositorio<T> where T : class
+    {
+        //Contrato donde unicamente se declaran los metodos
+        //Crear recibe la entidad tipo T
+        Task Crear(T entidad);
+
+        //retorna una lista segun la entidad enviada
+        Task<List<T>> ObtenerTodos(Expression<Func<T,bool>>? filtro = null);
+
+        Task<T> Obtener(Expression<Func<T, bool>>? filtro = null, bool tracked = true);
+
+        Task<T> Remover(T entidad);
+
+        Task<T> Grabar();
+    }
+}
--- MagicVilla_API/obj/Debug/net7.0/MagicVilla_API.AssemblyInfo.cs
+++ MagicVilla_API/obj/Debug/net7.0/MagicVilla_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0912054a550c7f1fb3d9d79180f5f3f01840d803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
